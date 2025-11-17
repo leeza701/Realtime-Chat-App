00c2b056fb1fb5af7ad20aa6a6af466b3478e42d
leeza701 <leezamalik789@gmail.com>
2025-11-17 16:50:37 +0530

    added render link to axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance=axios.create({
    // baseURL:import.meta.env.MODE==="development"?"http://localhost:3001/api":"/api",
-    baseURL:"http://localhost:3001/api",
+    baseURL:"https://realtime-chat-app-2-7kbu.onrender.com",
     withCredentials:true,
 
 });
